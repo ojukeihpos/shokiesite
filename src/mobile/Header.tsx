e8c105c3357d77b29c630f788df9e65f2ef42cfa
Sophie Kujo <ojukeihpos@gmail.com>
2023-11-16 22:04:15 -0500

    Fixed the side bar a bit, but still allowed clickthrough to the content behind it. Probably a property I'm missing in the react-burger-menu docs

--- src/mobile/Header.tsx
+++ src/mobile/Header.tsx
@@ -8,18 +8,13 @@ import { slide as Slide } from 'react-burger-menu';
 
 export class Header extends React.Component<{}> {
     render() {
-        return <div className='shoka__header mobile'>
-            <SideBar pageWrapId='page-wrap' outerContainerId='outer-container' />
-            <a href="/" className='headerTitle'>Sophie</a>
+        return <div>
+            <Slide pageWrapId={'page-wrap'} outerContainerId={'outer-container'} width={'40%'}>
+                <div>test me!</div>
+            </Slide>
+            <div className='shoka__header mobile'>
+                <a href="/" className='headerTitle'>Sophie</a>
+            </div>
         </div>
-    }
-}
-
-class SideBar extends React.Component<{ pageWrapId: string, outerContainerId: string }> {
-
-    render() {
-        return <Slide>
-            <div>test!</div>
-        </Slide>
     }
 }
