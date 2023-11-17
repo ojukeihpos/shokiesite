32daa2d92f6cd51d0e5038d203264e1d81408f45
Sophie Kujo <ojukeihpos@gmail.com>
2023-11-16 23:50:12 -0500

    Changed the routing to always go to Content, since there's nothing else yet.

--- src/mobile/MobileComponent.tsx
+++ src/mobile/MobileComponent.tsx
@@ -11,8 +11,9 @@ export class MobileComponent extends React.Component<{}, {}> {
             <Header />
             <Background />
             <Routes>
-                <Route path="/" element={<Content />} />
-                <Route path="*" element={<div style={{ position: "absolute", top: "2.5rem", left: "50%", transform: "translateX(-50%)" }}>sorry, nothing</div>} />
+                <Route path="*" element={<Content />} />
+                {//<Route path="*" element={<div style={{ position: "absolute", top: "2.5rem", left: "50%", transform: "translateX(-50%)" }}>sorry, nothing</div>} />
+                }
             </Routes>
         </div>
     }
