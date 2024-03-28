ecbd6d5aea927bd9f146f3b7d8ab842c8c109b06
Sophie Kujo <ojukeihpos@gmail.com>
2024-03-28 08:59:25 -0400

    Added a quick note for latest work

--- README.md
+++ README.md
@@ -3,44 +3,13 @@
 
 Personal website that acts as an information display and portfolio.
 
-
 ## Used By
 
 https://shokie.xyz
-## Authors
-
-- Sophie
 
 
 ## Tech Stack
 
-**Client:** React, Router v6, CSS
+**Client:** React, TypeScript
 
-**Server:** NodeJS, Netlify, Domain hosting service (Namecheap)
-
-## Lessons Learned
-
-CSS:
-- Animation
-- Filtering
-
-Router:
-- Migration from v5 to v6
-## Color Reference
-
-| Color             | Hex                                                                |
-| ----------------- | ------------------------------------------------------------------ |
-| Text | ![#b7fbff](https://via.placeholder.com/10/b7fbff?text=+) rgba(#b7fbff) |
-| Clickable Text | ![#c749ab](https://via.placeholder.com/10/c749ab?text=+) #c749ab |
-| Screen Background | ![#35335a](https://via.placeholder.com/10/35335a?text=+) #35335a |
-
-
-## Acknowledgements
-Art:
- - [ultineet](https://twitter.com/ultineet)
- - [ChouGokuOozaru](https://twitter.com/ChouGokuOozaru)
-
-Website Design Choices:
- - [Vincent Nguyen](https://github.com/nvplus/)
- - [CedarSkye](https://github.com/cedarskye/)
-
+**Server:** NodeJS, Amazon Web Services
--- src/desktop/Professional.tsx
+++ src/desktop/Professional.tsx
@@ -41,14 +41,14 @@ export default class Professional extends React.Component<{}, {}> {
                     </div>*/}
                 <div className='about'>
                     <p>Hello! I'm Sophie, a software developer. I currently work as a Freelance Web Developer.</p>
-                    <br />
                     <h1>Career</h1>
+                    <p>My latest work can be seen at <a className='link' href="https://soarer.cloud">soarer.cloud</a>. I replicated the client's previous website with Next.js and Tailwind CSS, adding modern design and functionality while maintaining aesthetic decisions.</p>
+                    <img src="soarer.png" width="700"/>
                     <p>My formal experience began as a Undergraduate Research Assistant Full-Stack Developer at Wilfrid Laurier University.<br />
                         I took over and built upon a TypeScript React website, which was used across two semesters of over 30 students each. The website was a game-ified simulation of a transport market, where students acted as either a distributor or a transporter. They would then try to create and bid on contracts in order to maximize their own profits.<br /></p>
                     <br />
                     <p>Afterwards, I worked as a Technical Consultant for Gaijin Goombah LLC, where I optimized content production workflow and provided asset recommendations based on budget and functional restraints.<br />
                         I reduced file redundancy by implementing cloud storage solutions, which in turn facilitated their transition from local to remote work.</p>
-                    <br />
                     <h1>Other</h1>
                     <p>I have attended hackathons (<a className='link' href='https://devpost.com/captainkesty'>devpost</a>), notably earning an award at <a className="link" href="https://devpost.com/software/healthsimple">{"<Br/eak>Inequality"}</a> for best pitch. The objective for that hackathon was to create a product that would make access to medical resources easier to those in developing countries. My group decided to create a service, accessible through either SMS or low-spec Android app, that would let a user input symptoms and receive medical information.</p>
                     <br />
