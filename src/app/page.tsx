3af9e64f989477e5ddb12c74f35b71baa7a34fc9
sophie <ojukeihpos@gmail.com>
2024-12-04 18:54:43 -0500

    AWS parser is apparently not happy about unenclosed square brackets so I had to wrap it in a string. Oops

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return <div className="lg:hidden flex flex-col min-h-full h-screen bg-[#3c3d37]">
     <div id='mobile-main-content' className="mt-[3em] mx-auto">
       <div>Hello!</div>
-      <div>This is the mobile version of my website. It's admittedly a little barebones from the React -> NextJS port. Take a look around at what's been brought over, and there'll be more soon!</div>
+      <div>This is the mobile version of my website. It's admittedly a little barebones from the React {'->'} NextJS port. Take a look around at what's been brought over, and there'll be more soon!</div>
     </div>
   </div>
 }
