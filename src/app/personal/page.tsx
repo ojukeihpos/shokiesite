9b33abc4e30fb51608450e9bdfa3ebb0f1c074ae
sophie <ojukeihpos@gmail.com>
2025-01-25 13:19:38 -0500

    Content changes

--- src/app/personal/page.tsx
+++ src/app/personal/page.tsx
@@ -5,21 +5,25 @@ const pageContent = <><div id='titlehead'>
   Personal
   <hr className='profilehr' />
 </div><div className='about'>
-    <p>Hello! I'm Sophie. My pronouns are she/her.</p>
-    <br />
-    <p>I identify as LGBTQ+. I was co-president of Laurier Pride Society at Wilfrid Laurier University, and was one of the founding members.</p>
-    <br />
-    <p>I'm also a big Riichi Mahjong fan! I've been playing for multiple years on MahjongSoul, Final Fantasy XIV, and Tenhou. I've only recently started entering tournaments.</p>
-    <br />
-    <p>I also cosplay occasionally! Here's a list of the cosplays I've done:</p>
-    <ul id="cosplay-list">
-      <li>Reimu Hakurei (Touhou Project)</li>
-      <li>Yor Forger (Spy x Family)</li>
-      <li>Lady (Devil May Cry)</li>
-      <li>Maid (Final Fantasy XIV)</li>
-      <li>Motoko Kusanagi (Ghost in the Shell)</li>
-    </ul>
-    <br />
+    <p>Hello! I'm Sophie. I'm known for a lot of different things, so I'll do my best to try and cover all my interests below.</p>
+    <div>
+      <p className='profilehr text-4xl dark:text-white pt-[1.5rem] underline text-center'>
+        Mahjong
+      </p>
+      <div className='my-2'>
+        <p>I started learning Mahjong over the pandemic, and have since become a player at various online and offline locations. I've provided streaming and commentating services to <span className='italic'>The NA Online Mahjong Ladder</span>, an online community that hosts Mahjong tournaments.</p>
+      </div>
+    </div>
+    <p className='profilehr text-4xl dark:text-white pt-[1.5rem] underline text-center'>
+      Fighting Games
+    </p>
+    <p>I've been involved in the Fighting Game Community for as long as I can remember. I got my start with Street Fighter IV, and since then, my love for the genre has lead me to discover new friendships both internationally and locally. I frequented the <span className='italic'>University of Waterloo Fighting Game Club</span> during my time in university, and have traveled to the US for events.</p>
+    <p>I got the opportunity to work under <Link href={"https://www.consolegamingleague.com/"} className='clickable link'><span className='italic'>Console Gaming League (CGL)</span></Link> as a tournament organizer and administration. While short lived, it was where I got my bearings and understanding of tournament workings.</p>
+    <p>I also got the opportunity to work for <Link href={"https://incendium.gg/"} className='clickable link'><span className='italic'>Incendium Gaming</span></Link> doing data entry (and, unofficially, a lot of other administrative duties) for Electric Clash 2024.</p>
+    <p className='profilehr text-4xl dark:text-white pt-[1.5rem] underline text-center'>
+      Cosplay
+    </p>
+    <p>There's not a lot to write about here for cosplay, because it's (unsurprisingly) a very visual medium. I like to cosplay for events, and I try to keep things fresh by not doing the same thing twice.</p>
   </div></>
 
 export default function Home() {
--- src/app/professional/page.tsx
+++ src/app/professional/page.tsx
@@ -22,15 +22,15 @@ const pageContent = <><div className='shoka__header'>
                     <p>Freelance Web Developer</p> <p> 03/2023 - Present</p>
                 </span>
                 <p>Online</p>
-                <div className='border-l-4 border-black dark:border-blue-400 pl-4 my-2'>Skills: Next.js, TypeScript, Node.js, Amazon Web Services (AWS), Git, Figma</div>
+                <div className='border-l-4 border-blue-400 pl-4 my-2'>Skills: Next.js, TypeScript, Node.js, Amazon Web Services (AWS), Git, Figma</div>
             </div>
             <div className='border-l-4 border-black dark:border-[#dad2c7] pl-4'>
-                <p>- Custom-coded websites with a technological familiarity in React-based frameworks using TypeScript and Tailwind.</p>
-                <p>- Provided fast updates and technical support for any client needs.</p>
+                <p>- Developed custom-coded websites with a technological familiarity in React-based frameworks using TypeScript and Tailwind</p>
+                <p>- Provided proactive progress updates and all-encompassing technical support for any client needs</p>
             </div>
             <div className='border-l-4 border-[#c93e25] pl-4 my-2'>
-                <p>My latest work can be seen at <a className='link' href="https://soarer.cloud">soarer.cloud</a>. I replicated the client's previous website with Next.js and Tailwind CSS, adding modern design and functionality while maintaining aesthetics.</p>
-                <p>The latest addition to said website is a live feed of their BlueSky profile, utilizing a new technology referred to as the AT Protocol.</p>
+                <p>Latest work can be seen at <a className='link' href="https://soarer.cloud">soarer.cloud</a>. Replicated the client's previous website with Next.js and Tailwind CSS, adding modern design and functionality while maintaining aesthetics</p>
+                <p>Newest addition to said website is a live feed of a BlueSky profile, utilizing a new technology referred to as the AT Protocol</p>
             </div>
         </div>
 
@@ -40,11 +40,11 @@ const pageContent = <><div className='shoka__header'>
                     <p>Technical Consultant</p> <p>02/2022 - 01/2023, 12/2023 - 01/2025</p>
                 </span>
                 <p>Gaijin Goombah LLC</p>
-                <div className='border-l-4 border-black dark:border-blue-400 pl-4 my-2'>Skills: Cloud Storage, System Design, Computer Hardware</div>
+                <div className='border-l-4 border-blue-400 pl-4 my-2'>Skills: Cloud Storage, System Design, Computer Hardware</div>
             </div>
             <div className='border-l-4 border-black dark:border-[#dad2c7] pl-4'>
-                <p>- Optimized content production workflow and provided hardware recommendations based on technical requirements and budget.</p>
-                <p>- Reduced file redundancy by implementing cloud storage solutions and guidance on best practices to preserve hardware longevity.</p>
+                <p>- Optimized content production workflow and provided hardware recommendations based on technical requirements and budget</p>
+                <p>- Reduced file redundancy by implementing cloud storage solutions and guidance on best practices to preserve hardware longevity</p>
             </div>
         </div>
         <div className='content-segment py-[1rem]'>
@@ -53,25 +53,27 @@ const pageContent = <><div className='shoka__header'>
                     <p>Full-Stack Developer</p> <p> 03/2019 - 02/2020</p>
                 </span>
                 <p>Wilfrid Laurier University</p>
-                <div className='border-l-4 border-black dark:border-blue-400 pl-4 my-2'>Skills: React, TypeScript, Node.js, Express, MongoDB, Java, Git, AWS</div>
+                <div className='border-l-4 border-blue-400 pl-4 my-2'>Skills: React, TypeScript, Node.js, Express, MongoDB, Java, Git, AWS</div>
             </div>
             <div className='border-l-4 border-black dark:border-[#dad2c7] pl-4'>
-                <p>- Developed and maintained a TypeScript React application which was used across two semesters of a senior-level class.</p>
+                <p>- Developed and maintained a TypeScript React application which was used across two semesters of a senior-level class</p>
             </div>
-            <div className='border-l-4 border-[#c93e25] pl-4 my-2'>Game-ified simulation of a transport market, where students acted as either a distributor or a transporter. The objective of the game was to maximize profits through a series of contract negotiations.</div>
+            <div className='my-2'>
+                <div className='border-l-4 border-[#c93e25] pl-4'>Game-ified simulation of a transport market, where students acted as either a distributor or a transporter</div>
+                <div className='border-l-4 border-[#c93e25] pl-4'>Objective of the game was to maximize profits through a series of contract negotiations</div>
+            </div>
             <div className='border-l-4 border-black dark:border-[#dad2c7] pl-4'>
-                <p>- Executed MongoDB and domain hosting migration to university servers.</p>
-                <p>- Collaborated with cross-functional teams to assess technical challenges and communicate solutions.</p>
-                <p>- Developed a Java-based application for automating large-scale data processing and visualization to mitigate research delays.</p>
+                <p>- Executed MongoDB and domain hosting migration to university servers</p>
+                <p>- Collaborated with cross-functional teams to assess technical challenges and communicate solutions</p>
+                <p>- Developed a Java-based application for automating large-scale data processing and visualization to mitigate research delays</p>
             </div>
         </div>
         <p className='profilehr text-4xl dark:text-white underline text-center'>Other</p>
-        <p>- Attended hackathons (<a className='link' href='https://devpost.com/captainkesty'>devpost</a>), notably earning an award at <a className="link" href="https://devpost.com/software/healthsimple">{"<Br/eak>Inequality"}</a> for best pitch. The objective for that hackathon was to create a product that would make access to medical resources easier to those in developing countries. My group decided to create a service, accessible through either SMS or low-spec Android app, that would let a user input symptoms and receive medical information.</p>
-        <br />
-        <p>- Helped found a new hackathon, StartHacks, at Wilfrid Laurier University alongside the members of 「Computer Science, Physics, and Mathematics Club」 (PHI) at Laurier, now known as 「Laurier Computing Society」 (LCS). The hackathon set the groundwork for Laurier's future hackathons and allowed for future staff to understand the obstacles associated with running a large scale event.</p>
-        <br />
-        <p>- Contributed to open-source projects, such as <a className="link" href="https://github.com/DefinitelyTyped/DefinitelyTyped">DefinitelyTyped</a>. I added typings to <a className="link" href="https://github.com/cettoana/react-scramble">react-scramble</a>, which you can see in action by hovering over (if you're on desktop) the name on the top left of this page.</p>
-        <br />
+        <p className='border-l-4 border-black dark:border-[#dad2c7] pl-4 my-2'>- Helped found a new hackathon, <span className='italic'>StartHacks</span>, at Wilfrid Laurier University alongside the members of 「Computer Science, Physics, and Mathematics Club」 (PHI) at Laurier, now known as 「Laurier Computing Society」 (LCS). The hackathon set the groundwork for Laurier's future hackathons and allowed for future staff to understand the obstacles associated with running a large scale event</p>
+        <p className='border-l-4 border-black dark:border-[#dad2c7] pl-4 my-2'>- Founded and acted as Co-President for <span className='italic'>Laurier Pride Society</span>, a group primarily focused on spreading awareness and providing a safe space for queer students</p>
+        <p className='border-l-4 border-black dark:border-[#dad2c7] pl-4 my-2'>- Served as an accountant for the <span className='italic'>University of Waterloo Fighting Games Club</span>, keeping track of inventory and assisting with other executive functions</p>
+        <p className='border-l-4 border-black dark:border-[#dad2c7] pl-4 my-2'>- Attended hackathons (<a className='link' href='https://devpost.com/captainkesty'>devpost</a>), notably earning a Best Pitch award at <a className="link" href="https://devpost.com/software/healthsimple">{"<Br/eak>Inequality"}</a> for a low-spec app providing medical resources in areas of low access. Users could input symptoms and be given appropriate medical information in a simple and traditionally understood method (SMS or search engine)</p>
+        <p className='border-l-4 border-black dark:border-[#dad2c7] pl-4 my-2'>- Contributed to open-source projects, such as <a className="link" href="https://github.com/DefinitelyTyped/DefinitelyTyped">DefinitelyTyped</a> by adding typings to <a className="link" href="https://github.com/cettoana/react-scramble">react-scramble</a>. Can be seen in action by hovering over (if on desktop) the name on the top left of this page</p>
     </div></>
 
 export default function Professional() {
